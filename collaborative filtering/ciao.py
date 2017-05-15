006f79a3c896a6c0b48eb4c11bd4c7827ab280f3
potaty <potatyangel@sina.com>
2017-05-16 01:50:48 +0800

    finish ciao

--- collaborative filtering/ciao.py
+++ collaborative filtering/ciao.py
@@ -10,10 +10,12 @@ import scipy.io
 header = ['user_id', 'movie_id', 'genre_id', 'review_id', 'movie_rating', 'date']
 
 f = pd.read_csv('CIAO/movie-ratings.txt', sep = ',', names = header)
-print f
+t = pd.read_csv('CIAO/trusts.txt', sep = ',', names = ['u', 'v', 'trust'])
+
 
 n_users = f.user_id.unique().shape[0]
 n_items = f.movie_id.unique().shape[0]
+
 
 user_dict = {}
 movie_dict = {}
@@ -45,12 +47,12 @@ for line in train_data.itertuples():
 	user_dict[line[1] - 1][line[2] - 1] = 1
 	movie_dict[line[2] - 1][line[1] - 1] = 1
 
+
 for user in xrange(n_users):
 	predict_user[user] = sum(train_data_matrix[user]) / sum(map(lambda(x): 1 if x != 0 else 0, train_data_matrix[user]))
 
 for movie in xrange(n_items):
 	predict_movie[movie] = sum(train_data_matrix[:, movie]) / sum(map(lambda(x): 1 if x != 0 else 0, train_data_matrix[:, movie]))
-
 # calculate similarity
 # user_similarity = np.zeros((n_users, n_users))
 for u1 in xrange(n_users):
@@ -79,6 +81,12 @@ for i in xrange(n_users):
 			user_similarity[i][j] = fz[i][j] / math.sqrt(fm1[i][j]) / math.sqrt(fm2[i][j])
 			user_similarity[j][i] = user_similarity[i][j]
 
+for line in t.itertuples():
+	u = line[1] - 1
+	v = line[2] - 1
+	if u < n_users and v < n_users:
+		user_similarity[u][v] = 1
+
 # calculate RMSD
 result = 0.0
 
@@ -86,7 +94,7 @@ count = 0
 for line in test_data.itertuples():
 	user, movie, rating = line[1] - 1, line[2] - 1, line[5]
 	similarity = user_similarity[user].copy()
-	median = 0.2
+	median = np.median(similarity)
 	predict = 0.0
 	valid_ratings_count = sum(map(lambda(idx, x): x if train_data_matrix[idx][movie] > 0 and x > median else 0, enumerate(similarity)))
 	if valid_ratings_count != 0:
@@ -96,12 +104,10 @@ for line in test_data.itertuples():
 	if predict < 1.0 or predict != predict:
 		predict = predict_movie[movie]
 	if predict != predict:
-		predict = 4
+		predict = 3
 		count += 1
 	result += (predict - rating) ** 2
 	print predict, rating
 result /= len(test_data)
-print result
 result = math.sqrt(result)
 print result
-print count
